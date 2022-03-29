cb51c4d4f80327e679cdf5473fef8fbee94be3de
Artem Stepanov <stxima@gmail.com>
2022-03-29 16:45:24 +0300

    clean branch

--- src/main/kotlin/ru/stxima/openinghoursweb/service/openinghours/model/OpeningHoursDataServiceModels.kt
+++
@@ -1,7 +0,0 @@
-package ru.stxima.openinghoursweb.service.openinghours.model
-
-data class GetHumanReadableOpeningHoursFromRawDataRequestValidationResult(
-    val errorMessages: List<String>
-) {
-    val hasErrors = errorMessages.any()
-}
